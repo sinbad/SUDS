b35c29014056726a3c47437175e9ba2472fe99b2
Steve Streeting <steve@stevestreeting.com>
2022-10-19 12:21:49 +0100

    Added proof of concept parsing to module startup

--- Source/SUDSEditor/Private/SUDSEditor.cpp
+++ Source/SUDSEditor/Private/SUDSEditor.cpp
@@ -1,12 +1,30 @@
 ﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "SUDSEditor.h"
+#include "antlr4-runtime.h"
+#include "antlr_gen/ExpressionLexer.h"
+#include "antlr_gen/ExpressionParser.h"
 
 #define LOCTEXT_NAMESPACE "FSUDSModule"
 
 void FSUDSEditorModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	antlr4::ANTLRInputStream input("6*(2+3)");
+    
+	// Create a lexer from the input
+	ExpressionLexer lexer(&input);
+    
+	// Create a token stream from the lexer
+	antlr4::CommonTokenStream tokens(&lexer);
+    
+	// Create a parser from the token stream
+	ExpressionParser parser(&tokens);    
+
+	// Display the parse tree
+	// Note: use UTF8_TO_TCHAR(c_str()) for cases where there will be UTF8
+	FString Output(parser.expr()->toStringTree().c_str());
+	UE_LOG(LogTemp, Log, TEXT("%s"), *Output);
 }
 
 void FSUDSEditorModule::ShutdownModule()
