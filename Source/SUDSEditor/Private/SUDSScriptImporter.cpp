8c95c6f998700e72630643716e8b46ea13205f3a
Steve Streeting <steve@stevestreeting.com>
2022-10-21 16:46:49 +0100

    Detect header / body section lines

--- Source/SUDSEditor/Private/SUDSScriptImporter.cpp
+++ Source/SUDSEditor/Private/SUDSScriptImporter.cpp
@@ -1,5 +1,7 @@
 ﻿#include "SUDSScriptImporter.h"
 
+#include "SUDSEditor.h"
+
 bool FSUDSScriptImporter::ImportFromBuffer(const TCHAR *Start, int32 Length, const FString& NameForErrors, bool bSilent)
 {
 	static const TCHAR* LineEndings[] =
@@ -10,6 +12,10 @@ bool FSUDSScriptImporter::ImportFromBuffer(const TCHAR *Start, int32 Length, con
 	};
 	constexpr int32 NumDelims = UE_ARRAY_COUNT(LineEndings);
 
+	int LineNumber = 1;
+	bHeaderDone = false;
+	bHeaderInProgress = false;
+	bool bImportedOK = true;
 	if (Start)
 	{
 		int32 SubstringBeginIndex = 0;
@@ -38,7 +44,13 @@ bool FSUDSScriptImporter::ImportFromBuffer(const TCHAR *Start, int32 Length, con
 			{
 				const int32 SubstringLength = SubstringEndIndex - SubstringBeginIndex;
 				FStringView Line = MakeStringView(Start + SubstringBeginIndex, SubstringLength);
-				ParseLine(Line);
+				if (!ParseLine(Line, LineNumber++, NameForErrors, bSilent))
+				{
+					// Abort, error
+					bImportedOK = false;
+					break;
+				}
+				
 				
 				// Next substring begins at the end of the discovered delimiter.
 				SubstringBeginIndex = SubstringEndIndex + DelimiterLength;
@@ -53,27 +65,91 @@ bool FSUDSScriptImporter::ImportFromBuffer(const TCHAR *Start, int32 Length, con
 		// Add any remaining characters after the last delimiter.
 		const int32 SubstringLength = Length - SubstringBeginIndex;
 		FStringView Line = MakeStringView(Start + SubstringBeginIndex, SubstringLength);
-		ParseLine(Line);
+		bImportedOK = ParseLine(Line, LineNumber++, NameForErrors, bSilent);
 	}
 
-	// TODO: actually check
-	return true;
+	return bImportedOK;
 	
 }
 
-void FSUDSScriptImporter::ParseLine(const FStringView& Line)
+bool FSUDSScriptImporter::ParseLine(const FStringView& Line, int LineNo, const FString& NameForErrors, bool bSilent)
 {
-	if (Line.Len() == 0)
+	if (Line.Len() == 0 && !bTextInProgress)
 	{
 		// We will skip any blank lines that aren't inside text
-		
+		return true;
 	}
 
+	// Check for headers
+	static const FStringView HeaderPrefix(TEXT("==="));
+	if (Line.StartsWith(HeaderPrefix))
+	{
+		if (bHeaderDone)
+		{
+			if (!bSilent)
+			{
+				UE_LOG(LogSUDSEditor, Warning, TEXT("Failed to parse %s Line %d: Duplicate header section"), *NameForErrors, LineNo);
+			}
+			return false;
+		}
+		
+		if (bHeaderInProgress)
+		{
+			// End of header
+			bHeaderInProgress = false;
+			bHeaderDone = true;
+		}
+		else
+		{
+			bHeaderInProgress = true;
+		}
+		return true;
+	}
+	else if (bHeaderInProgress)
+	{
+		return ParseHeaderLine(Line, LineNo, NameForErrors, bSilent);
+	}
+
+	// Process body
+
+	UE_LOG(LogSUDSEditor, Log, TEXT("%d: BODY  : %s"), LineNo, *FString(Line));
+	
 	// Trim off any leading whitespace, but record how much of it there is since it's relevant
 	// If less than current indent, pop context from stack
 	// If more than current indent, then a child of previous line
 	// If same as current indent, continuation of current context
-	FStringView LeadingWS = Line.TrimStart();
+	int IndentLevel;
+	FStringView Trimmed = TrimLine(Line, &IndentLevel);
+
+
+	return true;
 	
 }
 
+bool FSUDSScriptImporter::ParseHeaderLine(const FStringView& Line, int LineNo, const FString& NameForErrors, bool bSilent)
+{
+	// TODO parse header content
+
+	UE_LOG(LogSUDSEditor, Log, TEXT("%d: HEADER: %s"), LineNo, *FString(Line));
+	return true;
+}
+
+FStringView FSUDSScriptImporter::TrimLine(const FStringView& Line, int* OutIndentLevel) const
+{
+	OutIndentLevel = 0;
+	int32 SkippedChars = 0;
+	for (const TCHAR Char : Line)
+	{
+		if (!TChar<TCHAR>::IsWhitespace(Char))
+		{
+			break;
+		}
+		++SkippedChars;
+		if (Char == '\t')
+			OutIndentLevel += TabIndentValue;
+		else
+			++OutIndentLevel;
+	}
+	return FStringView(Line.GetData() + SkippedChars, Line.Len() - SkippedChars);
+	
+}
--- Source/SUDSEditor/Public/SUDSScriptImporter.h
+++ Source/SUDSEditor/Public/SUDSScriptImporter.h
@@ -11,7 +11,13 @@ public:
 	bool ImportFromBuffer(const TCHAR* Buffer, int32 Len, const FString& NameForErrors, bool bSilent);
 
 protected:
+	const int TabIndentValue = 4;
+	bool bHeaderDone = false;
+	bool bHeaderInProgress = false;
+	bool bTextInProgress = false;
 	/// Parse a single line
-	void ParseLine(const FStringView& Line);
+	bool ParseLine(const FStringView& Line, int LineNo, const FString& NameForErrors, bool bSilent);
+	bool ParseHeaderLine(const FStringView& Line, int LineNo, const FString& NameForErrors, bool bSilent);
+	FStringView TrimLine(const FStringView& Line, int* OutIndentLevel) const;
 
 };
