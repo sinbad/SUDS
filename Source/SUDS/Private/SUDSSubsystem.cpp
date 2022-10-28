4365b1ec1f412f226028650b6ded5bb16c589d5a
Steve Streeting <steve@stevestreeting.com>
2022-10-28 11:11:53 +0100

    Boilerplate subsystem

---
+++ Source/SUDS/Private/SUDSSubsystem.cpp
@@ -0,0 +1,14 @@
+﻿#include "SUDSSubsystem.h"
+
+
+DEFINE_LOG_CATEGORY(LogSUDSSubsystem)
+
+void USUDSSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void USUDSSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
---
+++ Source/SUDS/Public/SUDSSubsystem.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SUDSSubsystem.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogSUDSSubsystem, Log, All);
+/**
+ * 
+ */
+UCLASS()
+class SUDS_API USUDSSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+};
+
+inline USUDSSubsystem* GetSUDSSubsystem(UWorld* WorldContext)
+{
+	if (IsValid(WorldContext) && WorldContext->IsGameWorld())
+	{
+		auto GI = WorldContext->GetGameInstance();
+		if (IsValid(GI))
+			return GI->GetSubsystem<USUDSSubsystem>();		
+	}
+		
+	return nullptr;
+}
