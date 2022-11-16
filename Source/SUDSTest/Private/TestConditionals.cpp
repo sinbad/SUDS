e957dd6bd93e9abb7cc7b5677675fe6464998ba6
Steve Streeting <steve@stevestreeting.com>
2022-11-16 10:47:25 +0000

    Test falling through to another select node

--- Source/SUDSTest/Private/TestConditionals.cpp
+++ Source/SUDSTest/Private/TestConditionals.cpp
@@ -14,7 +14,12 @@ Player: Hello
 [else]
     NPC: Reply when x is something else
 [endif]
-Player: the end
+[if {z} == true]
+    Player: the end is true
+[else]
+    Player: the end is false
+[endif]
+NPC: OK
 )RAWSUD";
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTestBasicConditionals,
@@ -50,23 +55,39 @@ bool FTestBasicConditionals::RunTest(const FString& Parameters)
             TestParsedSelectEdge(this, "Nested select edge 1", SelectNode2, 0, "{y} == 1", Importer, &NextNode);
             TestParsedText(this, "Nested node edge 1", NextNode, "Player", "Player text when x ==1 and y == 1");
             TestGetParsedNextNode(this, "Get next", NextNode, Importer, false, &NextNode);
-            TestParsedText(this, "Nested node edge fallthrough", NextNode, "Player", "the end");
+            if (TestParsedSelect(this, "Fallthrough select", NextNode, 2))
+            {
+                auto SelectNode3 = NextNode;
+                TestParsedSelectEdge(this, "Final select edge 1", SelectNode3, 0, "{z} == true", Importer, &NextNode);
+                TestParsedText(this, "Final select edge 1 text", NextNode, "Player", "the end is true");
+                TestGetParsedNextNode(this, "Get next", NextNode, Importer, false, &NextNode);
+                TestParsedText(this, "Final fallthrough", NextNode, "NPC", "OK");
+
+                TestParsedSelectEdge(this, "Final select edge 2", SelectNode3, 1, "", Importer, &NextNode);
+                TestParsedText(this, "Final select edge 2 text", NextNode, "Player", "the end is false");
+                TestGetParsedNextNode(this, "Get next", NextNode, Importer, false, &NextNode);
+                TestParsedText(this, "Final fallthrough", NextNode, "NPC", "OK");
+                
+            }
 
             // Go back to the nested select
             // This "else" edge should have been created automatically to fall through 
             TestParsedSelectEdge(this, "Nested select edge 2", SelectNode2, 1, "", Importer, &NextNode);
-            TestParsedText(this, "Nested node edge fallthrough", NextNode, "Player", "the end");
+            // Just test it gets to the fallthrough, we've already tested the continuation from there
+            TestParsedSelect(this, "Fallthrough select", NextNode, 2);
             
         }
         TestParsedSelectEdge(this, "First select edge 2 (elseif)", SelectNode, 1, "{x} == 2", Importer, &NextNode);
         TestParsedText(this, "Select node 2", NextNode, "NPC", "Reply when x == 2");
         TestGetParsedNextNode(this, "Get next", NextNode, Importer, false, &NextNode);
-        TestParsedText(this, "Elseif node edge fallthrough", NextNode, "Player", "the end");
+        // Just test it gets to the fallthrough, we've already tested the continuation from there
+        TestParsedSelect(this, "Fallthrough select", NextNode, 2);
         
         TestParsedSelectEdge(this, "First select edge 2 (else)", SelectNode, 2, "", Importer, &NextNode);
         TestParsedText(this, "Select node 3", NextNode, "NPC", "Reply when x is something else");
         TestGetParsedNextNode(this, "Get next", NextNode, Importer, false, &NextNode);
-        TestParsedText(this, "Else node edge fallthrough", NextNode, "Player", "the end");
+        // Just test it gets to the fallthrough, we've already tested the continuation from there
+        TestParsedSelect(this, "Fallthrough select", NextNode, 2);
     }
     
 	return true;
