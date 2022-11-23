ed90968a68c64c257807addde71a467ece47fee2
Steve Streeting <steve@stevestreeting.com>
2022-11-23 14:18:00 +0000

    Testing running conditional choices

--- Source/SUDSTest/Private/TestConditionals.cpp
+++ Source/SUDSTest/Private/TestConditionals.cpp
@@ -65,6 +65,70 @@ NPC: Bye
 )RAWSUD";
 
 
+const FString MixedChoiceAndBranchInput = R"RAWSUD(
+[if {alreadyvisited}]
+    [goto secondvisit]
+[endif]
+
+NPC: Hello
+    * First choice
+        Player: I took the 1.1 choice
+[if {x} > 0]
+    * Second choice (conditional)
+        Player: I took the 1.2 choice
+    * Third choice (conditional)
+        Player: I took the 1.3 choice
+[else]
+    * Second Alt Choice
+        Player: I took the alt 1.2 choice        
+[endif]
+    * Common last choice
+        Player: I took the 1.4 choice
+
+[goto goodbye]
+
+:secondvisit
+
+NPC: Hello again
+
+[if {y} < 0]
+    Player: Y is less than 0
+    NPC: How interesting
+        * You don't sound that interested
+            NPC: Well I was trying to be nice
+        [if {x} == 0]
+            * Also x is zero
+                NPC: Fascinating
+        [endif]
+        * Well, better be off
+            [goto goodbye]
+[elseif {y} == 0]
+    Player: Y is zero
+    NPC: So is my interest level
+    Player: Rude
+    
+[else]
+    Player: Who knows what Y is anyway
+    [if {ponderous}]
+        * Who knows what anything is?
+            NPC: Get out
+    [endif]
+    [if {y} > 5]
+        * It's more than I can count on one hand
+            NPC: Well, that helps
+    [else]
+        * It's kind of small though
+            NPC: Much like your brain
+    [endif]
+        * I'm done with this
+[endif]
+
+Player: This is some fallthrough text
+
+:goodbye
+NPC: Bye
+)RAWSUD";
+
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTestBasicConditionals,
 								 "SUDSTest.TestBasicConditionals",
@@ -354,6 +418,57 @@ bool FTestConditionalChoices::RunTest(const FString& Parameters)
         
     }
     
+    
+    return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTestMixedChoiceAndBranch,
+                                 "SUDSTest.TestMixedChoiceAndBranch",
+                                 EAutomationTestFlags::EditorContext |
+                                 EAutomationTestFlags::ClientContext |
+                                 EAutomationTestFlags::ProductFilter)
+
+
+bool FTestMixedChoiceAndBranch::RunTest(const FString& Parameters)
+{
+    FSUDSScriptImporter Importer;
+    TestTrue("Import should succeed", Importer.ImportFromBuffer(GetData(MixedChoiceAndBranchInput), MixedChoiceAndBranchInput.Len(), "MixedChoiceAndBranchInput", true));
+
+    auto Script = NewObject<USUDSScript>(GetTransientPackage(), "Test");
+    const ScopedStringTableHolder StringTableHolder;
+    Importer.PopulateAsset(Script, StringTableHolder.StringTable);
+
+    // Script shouldn't be the owner of the dialogue but it's the only UObject we've got right now so why not
+    auto Dlg = USUDSLibrary::CreateDialogue(Script, Script);
+
+    // For the first run, do not set any state
+    Dlg->Start();
+
+    TestDialogueText(this, "Text node", Dlg, "NPC", "Hello");
+    TestEqual("Num choices", Dlg->GetNumberOfChoices(), 3);
+    TestEqual("Num choices", Dlg->GetChoiceText(0).ToString(), "First choice");
+    TestEqual("Num choices", Dlg->GetChoiceText(1).ToString(), "Second Alt Choice");
+    TestEqual("Num choices", Dlg->GetChoiceText(2).ToString(), "Common last choice");
+    TestTrue("Choose", Dlg->Choose(0));
+    TestDialogueText(this, "Text node", Dlg, "Player", "I took the 1.1 choice");
+    TestTrue("Continue", Dlg->Continue());
+    TestDialogueText(this, "Text node", Dlg, "NPC", "Bye");
+
+    // Choices are on first node so need to set before restart
+    Dlg->SetVariableInt("x", 10);
+    Dlg->Restart(false);
+    TestDialogueText(this, "Text node", Dlg, "NPC", "Hello");
+    TestEqual("Num choices", Dlg->GetNumberOfChoices(), 4);
+    TestEqual("Num choices", Dlg->GetChoiceText(0).ToString(), "First choice");
+    TestEqual("Num choices", Dlg->GetChoiceText(1).ToString(), "Second choice (conditional)");
+    TestEqual("Num choices", Dlg->GetChoiceText(2).ToString(), "Third choice (conditional)");
+    TestEqual("Num choices", Dlg->GetChoiceText(3).ToString(), "Common last choice");
+    TestTrue("Choose", Dlg->Choose(2));
+    TestDialogueText(this, "Text node", Dlg, "Player", "I took the 1.3 choice");
+    TestTrue("Continue", Dlg->Continue());
+    TestDialogueText(this, "Text node", Dlg, "NPC", "Bye");
+    
+
     
     return true;
 }
