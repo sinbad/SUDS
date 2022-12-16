460d1d78942d1c5aad59fa110eb85bf3e425d05c
Steve Streeting <steve@stevestreeting.com>
2022-12-16 16:16:23 +0000

    Add nested gosub test

--- Source/SUDSTest/Private/TestGotoGosub.cpp
+++ Source/SUDSTest/Private/TestGotoGosub.cpp
@@ -78,4 +78,85 @@ bool FTestGotoGosub::RunTest(const FString& Parameters)
 	return true;
 }
 
+
+const FString NestedGosubInput = R"RAWSUD(
+Player: Hello there
+[gosub sub1]
+NPC: Back at level 0
+[goto goodbye]
+
+:sub1
+Player: This is level 1
+[gosub sub2_1]
+[gosub sub2_2]
+Player: End of level 1
+[return]
+
+:sub2_1
+Player: This is level 2, sub 1
+[return]
+
+
+:sub2_2
+Player: This is level 2, sub 2
+NPC: We have to go deeper
+[gosub sub3]
+Player: End of level 2, sub 2
+[return]
+
+:sub3
+Player: This is level 3
+[return]
+
+:goodbye
+NPC: Bye!
+)RAWSUD";
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FTestNestedGosub,
+								 "SUDSTest.TestNestedGosub",
+								 EAutomationTestFlags::EditorContext |
+								 EAutomationTestFlags::ClientContext |
+								 EAutomationTestFlags::ProductFilter)
+
+
+
+bool FTestNestedGosub::RunTest(const FString& Parameters)
+{
+	FSUDSScriptImporter Importer;
+	TestTrue("Import should succeed", Importer.ImportFromBuffer(GetData(NestedGosubInput), NestedGosubInput.Len(), "NestedGosubInput", true));
+
+	auto Script = NewObject<USUDSScript>(GetTransientPackage(), "Test");
+	const ScopedStringTableHolder StringTableHolder;
+	Importer.PopulateAsset(Script, StringTableHolder.StringTable);
+
+	// Script shouldn't be the owner of the dialogue but it's the only UObject we've got right now so why not
+	auto Dlg = USUDSLibrary::CreateDialogue(Script, Script);
+	Dlg->Start();
+
+	
+	TestDialogueText(this, "Start node", Dlg, "Player", "Hello there");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "Player", "This is level 1");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "Player", "This is level 2, sub 1");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "Player", "This is level 2, sub 2");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "NPC", "We have to go deeper");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "Player", "This is level 3");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "Player", "End of level 2, sub 2");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "Player", "End of level 1");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Sub", Dlg, "NPC", "Back at level 0");
+	TestTrue("Continue", Dlg->Continue());
+	TestDialogueText(this, "Bye", Dlg, "NPC", "Bye!");
+	TestFalse("Continue", Dlg->Continue());
+
+	return true;
+}
+
+
 PRAGMA_ENABLE_OPTIMIZATION
