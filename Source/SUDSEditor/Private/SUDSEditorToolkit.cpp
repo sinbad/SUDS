6f2ad424af110e21bdf9a408268604fd92d08b0e
Steve Streeting <steve@stevestreeting.com>
2023-01-30 15:24:22 +0000

    Implemented resizeable columns in variables view via header row

--- Source/SUDSEditor/Private/SUDSEditorToolkit.cpp
+++ Source/SUDSEditor/Private/SUDSEditorToolkit.cpp
@@ -52,13 +52,45 @@ void FSUDSEditorToolkit::RegisterTabSpawners(const TSharedRef<FTabManager>& InTa
 
 	InTabManager->RegisterTabSpawner("SUDSDialogueTab", FOnSpawnTab::CreateLambda([=](const FSpawnTabArgs&)
 	{
-		
 		DialogueListView = SNew(SListView<TSharedPtr<FSUDSEditorDialogueRow>>)
 				.ItemHeight(24)
 				.ListItemsSource(&DialogueRows)
 				.OnGenerateRow(this, &FSUDSEditorToolkit::OnGenerateRowForDialogue);
 
 		ChoicesBox = SNew(SVerticalBox);
+
+		VariablesListView = SNew(SListView<TSharedPtr<FSUDSEditorVariableRow>>)
+				.ItemHeight(24)
+				.ListItemsSource(&VariableRows)
+				.OnGenerateRow(this, &FSUDSEditorToolkit::OnGenerateRowForVariable)
+				.HeaderRow(
+					SNew(SHeaderRow)
+					+ SHeaderRow::Column("NameHeader")
+					.FillWidth(0.40f)
+					[
+						SNew(SHorizontalBox)
+						+SHorizontalBox::Slot()
+						.AutoWidth()
+						.VAlign(VAlign_Center)
+						[
+							SNew( STextBlock )
+							.Text( INVTEXT("Name") )
+						]
+					]
+					+ SHeaderRow::Column("ValueHeader")
+					.FillWidth(0.40f)
+					[
+						SNew(SHorizontalBox)
+						+SHorizontalBox::Slot()
+						.AutoWidth()
+						.VAlign(VAlign_Center)
+						[
+							SNew( STextBlock )
+							.Text( INVTEXT("Value") )
+						]
+					]
+
+				);
 
 		
 		return SNew(SDockTab)
@@ -86,8 +118,7 @@ void FSUDSEditorToolkit::RegisterTabSpawners(const TSharedRef<FTabManager>& InTa
 		// Possibly use a SPropertyTable with a custom IPropertyTable to implement variable binding
 		return SNew(SDockTab)
 		[
-			SNew(STextBlock)
-			.Text(INVTEXT("Variables here"))
+			VariablesListView.ToSharedRef()
 		];
 	}))
 	.SetDisplayName(INVTEXT("Variables"))
@@ -187,6 +218,7 @@ void FSUDSEditorToolkit::StartDialogue()
 {
 	DialogueRows.Empty();
 	DialogueListView->RequestListRefresh();
+	VariableRows.Empty();
 	if (!Dialogue)
 	{
 		Dialogue = USUDSLibrary::CreateDialogue(nullptr, Script);
@@ -205,6 +237,12 @@ void FSUDSEditorToolkit::StartDialogue()
 
 	}
 	Dialogue->Restart(true);
+
+	// TODO: query variables
+	VariableRows.Add(MakeShareable(new FSUDSEditorVariableRow(FName("Test1"), FSUDSValue(true))));
+	VariableRows.Add(MakeShareable(new FSUDSEditorVariableRow(FName("TestVar2"), FSUDSValue(23))));
+	VariablesListView->RequestListRefresh();
+	
 }
 
 void FSUDSEditorToolkit::OnDialogueChoice(USUDSDialogue* D, int ChoiceIndex)
@@ -290,19 +328,104 @@ TSharedRef<ITableRow> FSUDSEditorToolkit::OnGenerateRowForDialogue(
 
 			+ SHorizontalBox::Slot()
 			.AutoWidth()
-			.Padding(0)
+			.Padding(10, 5, 10, 5)
 			[
 				SNew(STextBlock)
 				.Text(Row->SpeakerName)
+				.ColorAndOpacity(FSlateColor(FLinearColor::Yellow))
 			]
 
 			+ SHorizontalBox::Slot()
 			.AutoWidth()
-			.Padding(0)
+			.Padding(10, 5, 10, 5)
 			[
 				SNew(STextBlock)
 				.Text(Row->Line)
 			]
 		];
 		
+}
+
+TSharedRef<ITableRow> FSUDSEditorToolkit::OnGenerateRowForVariable(TSharedPtr<FSUDSEditorVariableRow> Row,
+	const TSharedRef<STableViewBase>& Table)
+{
+
+	return SNew( SSUDSEditorVariableItem, Table )
+		.VariableName(Row->Name)
+		.VariableValue(Row->Value)
+		.InitialWidth( VarColumnWidth );
+	
+}
+
+void SSUDSEditorVariableItem::Construct(const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView)
+{
+	InitialWidth = InArgs._InitialWidth;
+	VariableName = InArgs._VariableName;
+	VariableValue = InArgs._VariableValue;
+
+	SMultiColumnTableRow< TSharedPtr< FString > >::Construct( SMultiColumnTableRow< TSharedPtr< FString > >::FArguments(), InOwnerTableView );
+}
+
+TSharedRef<SWidget> SSUDSEditorVariableItem::GenerateWidgetForColumn(const FName& ColumnName)
+{
+	if (ColumnName == "NameHeader")
+	{
+		return SNew(SHorizontalBox)
+
+		+ SHorizontalBox::Slot()
+		.MaxWidth(InitialWidth) // Just the initial width. SMultiColumnTableRow updates it based on header width
+		.FillWidth(1.0)
+		.Padding(10, 5, 10, 5)
+		[
+			SNew(STextBlock)
+			.Text(FText::FromName(VariableName))
+		];
+		
+	}
+	else
+	{
+		// TODO: editable widgets
+		TSharedPtr<SWidget> ValueWidget;
+		switch (VariableValue.GetType())
+		{
+		case ESUDSValueType::Int:
+			ValueWidget = SNew(STextBlock)
+			.Text(FText::Format(INVTEXT("{0}"), VariableValue.GetIntValue()));
+			break;
+		case ESUDSValueType::Float:
+			ValueWidget = SNew(STextBlock)
+			.Text(FText::Format(INVTEXT("{0}"), VariableValue.GetFloatValue()));
+			break;
+		case ESUDSValueType::Boolean:
+			ValueWidget = SNew(STextBlock)
+			.Text(VariableValue.GetBooleanValue() ? FText::FromString("True") : FText::FromString("False"));
+			break;
+		case ESUDSValueType::Gender:
+			ValueWidget = SNew(STextBlock)
+			.Text(FText::Format(INVTEXT("{0}"), VariableValue.GetGenderValue()));
+			break;
+		case ESUDSValueType::Text:
+			ValueWidget = SNew(STextBlock)
+			.Text(VariableValue.GetTextValue());
+			break;
+		case ESUDSValueType::Name:
+			ValueWidget = SNew(STextBlock)
+			.Text(FText::FromString(VariableValue.GetNameValue().ToString()));
+			break;
+		case ESUDSValueType::Variable:
+		case ESUDSValueType::Empty:
+		default:
+			ValueWidget = SNew(STextBlock);
+			break;
+			
+		};
+		return SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			.Padding(10, 5, 10, 5)
+			[
+				ValueWidget.ToSharedRef()
+			];
+	}
+	
 }
--- Source/SUDSEditor/Private/SUDSEditorToolkit.h
+++ Source/SUDSEditor/Private/SUDSEditorToolkit.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
+#include "SUDSValue.h"
 #include "UObject/Object.h"
 
 struct FSUDSValue;
@@ -44,6 +45,47 @@ public:
 	FSUDSEditorDialogueRow(const FText& InSpeaker, const FText& InLine) : SpeakerName(InSpeaker), Line(InLine) {}
 };
 
+class FSUDSEditorVariableRow
+{
+public:
+	FName Name;
+	FSUDSValue Value;
+	FSUDSEditorVariableRow(const FName& InName, const FSUDSValue& InValue) : Name(InName), Value(InValue) {}
+};
+
+class SSUDSEditorVariableItem : public SMultiColumnTableRow< TSharedPtr<FString> >
+{
+public:	
+	SLATE_BEGIN_ARGS(SSUDSEditorVariableItem)
+	{}
+	SLATE_ARGUMENT(float, InitialWidth)
+	SLATE_ARGUMENT(FName, VariableName)
+	SLATE_ARGUMENT(FSUDSValue, VariableValue)
+SLATE_END_ARGS()
+
+public:
+
+	/**
+	 * Construct this widget.  Called by the SNew() Slate macro.
+	 *
+	 * @param	InArgs	          Declaration used by the SNew() macro to construct this widget.
+	 * @oaram   InOwnerTableView  The owner table into which this row is being placed.
+	 */
+	void Construct( const FArguments& InArgs, const TSharedRef<STableViewBase>& InOwnerTableView );
+
+	/**
+	 * Generates the widget for the specified column.
+	 *
+	 * @param ColumnName The name of the column to generate the widget for.
+	 * @return The widget.
+	 */
+	virtual TSharedRef<SWidget> GenerateWidgetForColumn( const FName& ColumnName ) override;
+protected:
+	float InitialWidth = 70;
+	FName VariableName;
+	FSUDSValue VariableValue;
+};
+
 class FSUDSEditorToolkit : public FAssetEditorToolkit
 {
 public:
@@ -63,10 +105,13 @@ protected:
 private:
 	USUDSScript* Script = nullptr;
 	USUDSDialogue* Dialogue = nullptr;
+	float VarColumnWidth = 75;
 	// FSUDSEditorDialogueRow needs to held by a TSharedPtr for SListView
 	TSharedPtr<SListView<TSharedPtr<FSUDSEditorDialogueRow>>> DialogueListView;
 	TArray<TSharedPtr<FSUDSEditorDialogueRow>> DialogueRows;
 	TSharedPtr<SVerticalBox> ChoicesBox;
+	TSharedPtr<SListView<TSharedPtr<FSUDSEditorVariableRow>>> VariablesListView;
+	TArray<TSharedPtr<FSUDSEditorVariableRow>> VariableRows;
 
 	void ExtendToolbar(FToolBarBuilder& ToolbarBuilder, TWeakPtr<SDockTab> Tab);
 	void StartDialogue();
@@ -83,6 +128,8 @@ private:
 	TSharedRef<ITableRow> OnGenerateRowForDialogue(
 		TSharedPtr<FSUDSEditorDialogueRow> FsudsEditorDialogueRow,
 		const TSharedRef<STableViewBase>& TableViewBase);
+	TSharedRef<ITableRow> OnGenerateRowForVariable(TSharedPtr<FSUDSEditorVariableRow> Row,
+	                                               const TSharedRef<STableViewBase>& Table);
 
 };
 
