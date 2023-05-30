828962bcd0fcace2c1b56423ba97aec86bc1ef07
Steve Streeting <steve@stevestreeting.com>
2023-05-30 14:51:51 +0100

    Fix highlighting numbers in speaker text, and runaway highlighting on plurals

--- Extras/PygmentsLexer/sudslexer/lexer.py
+++ Extras/PygmentsLexer/sudslexer/lexer.py
@@ -10,21 +10,22 @@ class SudsLexer(RegexLexer):
         'root': [
             (r'===\s*\n', Generic.Heading),
             # Choices
-            (r'\s*\*\s+[^\@\n]+[\@\n]', Generic.Subheading),
+            (r'\s*\*\s+[^\@\n]+[\@\n]', String),
             (r'\s*#.*\n', Comment),
             # Speaker lines
-            (r'\s*(\S+)(\:)', bygroups(Name.Class, Punctuation)),
+            (r'\s*\S+\:', Name.Class, 'speakerline'),
             # Close bracket for all special lines
             (r'\]', Operator),       
             (r'\b(and|or|&&|\|\||not)\b', Operator),
             # Set commands so we can highlight variable differently
             (r'(\[)\s*(set)(\s+)(\S+)', bygroups(Operator, Keyword, Text, Name.Variable)),
             (r'(\[)\s*(if|else|elseif|endif|event|return|goto|gosub|go to|go sub)\b', bygroups(Operator, Keyword)),
+            # Variables
             (r'(\{)([\w\.]+)(\})', bygroups(Operator, Name.Variable, Operator)),
-            (r'(\|)(plural|gender)(\()(.*)(\))', bygroups(Operator, Keyword, Operator, Keyword, Operator)),
-            (r'\b([tT]rue|[fF]alse|[mM]asculine|[fF]eminine|[nN]euter)\b', Keyword.Constant),
+            (r'(\|)(plural|gender)(\()(.*?)(\))', bygroups(Operator, Keyword, Operator, Keyword, Operator)),
+            (r'\b([tT]rue|[fF]alse|[mM]asculine|[fF]eminine|[nN]euter)\b', Name.Constant),
             (r'\+\/\-\*\!', Operator),
-            (r'\"[^\"]*\"', String.Double),
+            (r'\"[^\"]*\"', String.Single),
             (r'\`[^\`]*\`', String.Escape),
             (r'\d+(\.\d+)?', Number),
             # Line IDs
@@ -39,6 +40,18 @@ class SudsLexer(RegexLexer):
             (r'\s+[\n]?', Text),
             (r'\S+?[\n]?', Text), # non-greedy so we don't consume all non-whitespace
 
+
+        ],
+        # While in a speaker line, we ignore everything else except variables & markup
+        'speakerline' : [
+            # Variables
+            (r'(\{)([\w\.]+)(\})', bygroups(Operator, Name.Variable, Operator)),
+            (r'(\|)(plural|gender)(\()(.*?)(\))', bygroups(Operator, Keyword, Operator, Keyword, Operator)),
+            (r'[\@\n]', Text, '#pop'),
+            # Embedded markup
+            (r'\<\w+\>', Name.Decorator),
+            (r'\<\/\>', Name.Decorator),
+            (r'[^\@\n\<\{]+', Text),
 
         ]
     }
