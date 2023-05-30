1a72a5b6a50b47bd2c2bbf6732e805a7c4211ae0
Steve Streeting <steve@stevestreeting.com>
2023-05-30 16:01:40 +0100

    Fix event arguments not being correctly lexed

--- Extras/PygmentsLexer/setup.py
+++ Extras/PygmentsLexer/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup (
     name='sudslexer',
     packages=find_packages(),
-    version="0.0.7",
+    version="0.0.8",
     entry_points =
     """
     [pygments.lexers]
--- Extras/PygmentsLexer/sudslexer/lexer.py
+++ Extras/PygmentsLexer/sudslexer/lexer.py
@@ -60,7 +60,7 @@ class SudsLexer(RegexLexer):
             (r'\b([tT]rue|[fF]alse|[mM]asculine|[fF]eminine|[nN]euter)\b', Name.Constant),
             (r'\b(and|or|&&|\|\||not)\b', Operator),
             # Set, event commands so we can highlight variable/event differently
-            (r'\s*(set|event)(\s+)([^\]]+)', bygroups(Keyword, Text, Name.Variable)),
+            (r'\s*(set|event)(\s+)([^\]\s]+)', bygroups(Keyword, Text, Name.Variable)),
             (r'\s*(if|else|elseif|endif|event|return|goto|gosub|go to|go sub)\b', Keyword),
             (r'[,]', Punctuation),
             (r'\s+', Text), # whitespace OK
